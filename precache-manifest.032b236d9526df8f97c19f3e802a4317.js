e453dfda86bbb8de13932145e11d4f58feef950e
d3m1art <dimka.dolgiy@gmail.com>
2019-05-30 22:20:20 +0300

    Updates

---
+++ precache-manifest.032b236d9526df8f97c19f3e802a4317.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7bbdb2711d520ff129d",
+    "url": "/d3m1art/static/js/runtime~main.ece4f644.js"
+  },
+  {
+    "revision": "140fd919e30c6318d06a",
+    "url": "/d3m1art/static/js/main.faf9f576.chunk.js"
+  },
+  {
+    "revision": "3ed2366618856c5dc106",
+    "url": "/d3m1art/static/js/2.fe430265.chunk.js"
+  },
+  {
+    "revision": "140fd919e30c6318d06a",
+    "url": "/d3m1art/static/css/main.a34d88da.chunk.css"
+  },
+  {
+    "revision": "c82b463a79c4ebd06e7c8376ae5490fb",
+    "url": "/d3m1art/index.html"
+  }
+];
